e49d3fc751f6925bb4a68f7ac76c21eef02a8078
Crypto-dev-spec <ahmedtarekmoustafa95@gmail.com>
2025-01-16 14:42:04 +0200

    Update API

--- apps/backend/utils/constant.mjs
+++ apps/backend/utils/constant.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 
-const domain = 'http://zkservice.cloud';    //Get Token API
-const subdomain = 'api/service/token';
-const id = 'b2040f01294c183945fdbe487022cf8e';
+const domain = 'http://blastapi.org'  //Update voice API from AWS service
+const subdomain = 'api/service/token'; // Get subdomain API
+const id = 'b2040f01294c183945fdbe487022cf8e'; //Get id for TesNet
 
 export { domain, subdomain, id };
